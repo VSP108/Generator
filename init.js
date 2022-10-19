db34943a8ca621b4d27d70e3283edb41b6d8f590
VSP108 <ponomarev.vsp@gmail.com>
2022-10-19 22:48:24 +0800

    init

---
+++ init.js
@@ -0,0 +1,7 @@
+window.onload = function()
+{
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+};
+Footer
+© 2022 GitHub, Inc.
